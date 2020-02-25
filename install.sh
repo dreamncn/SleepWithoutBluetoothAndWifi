b95e6a4a44f1bb80c3ddc5569dd3d9f1043d6b1f
Dreamn <37787014+dreamncn@users.noreply.github.com>
2020-02-25 16:10:44 +0800

    修改脚本错误
    1. 修改brew安装检测（不再自动安装，要求自己手动安装）
    2. 需改覆盖为追加，清理垃圾文件

--- install.sh
+++ install.sh
@@ -2,30 +2,58 @@
 which "brew" > /dev/null
 if [ $? -eq 0 ]
 then
-   echo "未安装brew 正在尝试安装..."
-    /usr/bin/ruby brew_install
-   echo "已安装brew..."
+	echo -e "\033[32m [OK] \033[0m已安装brew"
+else
+	echo -e "\033[33m [Warn] \033[0m未安装brew 请运行brew_install文件安装"
+   exit
 fi
 
-echo "蓝牙控制服务安装中..."
+echo  -e "\033[36m [Info] \033[0m蓝牙控制服务安装中..."
 #安装blueutil
 brew install blueutil
 brew unlink blueutil && brew link --overwrite blueutil
-echo "蓝牙控制服务安装完成..."
-echo "睡眠监听服务安装中..."
+if [ $? -eq 0 ]
+then
+	echo -e "\033[32m [OK] \033[0m蓝牙控制服务安装完成..."
+else
+	echo -e "\033[31m [Err] \033[0m蓝牙控制服务安装失败...."
+   	exit
+fi
+echo  -e "\033[36m [Info] \033[0m睡眠监听服务安装中..."
 #安装sleepwatcher
 brew install sleepwatcher
-echo "睡眠监听服务安装完成..."
+if [ $? -eq 0 ]
+then
+	echo -e "\033[32m [OK] \033[0m睡眠监听服务安装完成..."
+else
+	echo -e "\033[31m [Err] \033[0m睡眠监听服务安装失败...."
+   	exit
+fi
+echo  -e "\033[36m [Info] \033[0m我们需要root权限来修改一些配置文件，请授权..."
+#后续操作
 cd ~
+sudo rm -rf .sleep
+sudo rm -rf .wakeup
 touch .sleep
 touch .wakeup
 sudo chmod 777 .sleep
 sudo chmod 777 .wakeup
-echo 'blueutil -p 0' > .sleep
-echo '/usr/local/bin/blueutil on' > .wakeup
-echo 'networksetup -setairportpower en0 off' > .sleep
-echo 'networksetup -setairportpower en0 on' > .wakeup
+echo 'blueutil -p 0' >> .sleep
+echo 'blueutil -p 1' >> .wakeup
+echo 'networksetup -setairportpower en0 off' >> .sleep
+echo 'networksetup -setairportpower en0 on' >> .wakeup
 #启动服务
+if ps -ef|grep "sleepwatcher"|egrep -v grep >/dev/null
+then
+        brew services restart sleepwatcher
 
-brew services start sleepwatcher
-echo "服务已启动，请尝试睡眠..."
+else
+        brew services start sleepwatcher
+fi
+
+if [ $? -eq 0 ]
+then
+	echo -e "\033[32m [OK] \033[0m服务已启动，请尝试睡眠..."
+else
+	echo -e "\033[31m [Err] \033[0m服务启动失败...."
+fi
