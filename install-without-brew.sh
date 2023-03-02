025eb291d862c2d9105be3240ffb6322dbe5cf7a
zihelyu <zihelyu@dawnwl.com>
2023-03-02 23:33:19 +0800

    修复安装后可能出现无法运行的问题
    Signed-off-by: zihelyu <zihelyu@dawnwl.com>

--- install-without-brew.sh
+++ install-without-brew.sh
@@ -3,15 +3,20 @@
 echo  -e "\033[36m [Info] \033[0m蓝牙控制服务安装中..."
 echo  -e "\033[36m [Info] \033[0m我们需要root权限来修改一些配置文件，请授权..."
 #安装blueutil
-sudo cp ./blueutil/2.7.0/bin/blueutil  /usr/local/bin/
+sudo mkdir -p /usr/local/bin
+sudo cp ./blueutil/2.7.0/bin/blueutil  /usr/local/bin
+sudo xattr -rc /usr/local/bin/blueutil
+sudo chmod 777 /usr/local/bin/blueutil
 echo -e "\033[32m [OK] \033[0m蓝牙控制服务安装完成..."
 
 echo  -e "\033[36m [Info] \033[0m睡眠监听服务安装中..."
 #安装sleepwatcher
 sudo mkdir -p /usr/local/sbin /usr/local/share/man/man8
 sudo cp ./sleepwatcher/2.2.1/sbin/sleepwatcher /usr/local/sbin
+sudo xattr -rc /usr/local/sbin/sleepwatcher
+sudo chmod 777 /usr/local/sbin/sleepwatcher
 sudo cp ./sleepwatcher/2.2.1/share/man/man8/sleepwatcher.8 /usr/local/share/man/man8
-sudo cp ./sleepwatcher/2.2.1/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist /Library/LaunchDaemons/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist 
+sudo cp ./sleepwatcher/2.2.1/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist /Library/LaunchAgents/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist 
 echo -e "\033[32m [OK] \033[0m睡眠监听服务安装完成..."
 #后续操作
 cd ~
@@ -35,6 +40,6 @@ do
 done
 EOF
 #启动服务
-sudo launchctl load /Library/LaunchDaemons/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist
+sudo launchctl load /Library/LaunchAgents/de.bernhard-baehr.sleepwatcher-20compatibility-localuser.plist
 
 echo -e "\033[32m [OK] \033[0m服务已启动，请尝试睡眠..."
